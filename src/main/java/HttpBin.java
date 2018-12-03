a26d9a1e11bf89bdd5eae25aacb70034f7cea291
AValerievna <a.valerievna12@gmail.com>
2018-12-03 23:17:05 +0300

    almost final version

--- src/main/java/HttpBin.java
+++ src/main/java/HttpBin.java
@@ -1,60 +1,57 @@
-import io.restassured.RestAssured;
 import io.restassured.http.Header;
 import io.restassured.response.Response;
-import io.restassured.specification.RequestSpecification;
 import org.json.JSONObject;
 
-import java.lang.reflect.Array;
 import java.util.*;
 
 import static io.restassured.RestAssured.*;
 
-public class HttpBin {
+class HttpBin {
+
+    private static final String BASE_URL = "http://httpbin.org/";
+    private static final String GET = "get";
+    private static final String POST = "post";
+    private static final String STREAM = "stream/{number}";
+    private static final String COLON = ":";
+    private static final String AUTHORIZATION = "Authorization";
+    private static final String BASIC = "Basic ";
+    private static final String BASIC_AUTH = "basic-auth/{validUsr}/{validPswd}";
 
     /**
      * Execute Get request
      */
-    public Response requestGet(Header header) {
-        return given().header(header).get("http://httpbin.org/get");
+    Response requestGet(Header header) {
+        return given().header(header).get(BASE_URL+ GET);
     }
 
-    public Response requestPost(HashMap<String,String> dataHashMap) {
-
-        JSONObject requestBody = new JSONObject();
-
-        for(Map.Entry<String,String> entry : dataHashMap.entrySet()) {
-            requestBody.put(entry.getKey(),entry.getValue());
-        }
-
-/*        requestBody.put("access-control-allow-credentials", "true");
-        requestBody.put("access-control-allow-origin:", "http://httpbin.org");
-        requestBody.put("connection:  ", "keep-alive");
-        requestBody.put("Password", "some");
-        requestBody.put("Email", "some" + "@gmail.com");*/
-        //RequestSpecification request = RestAssured.given();
-        //request.header("Accept", "application/json");
-      ;
-        //request.body(dataHashMap);
-
-
-        return   given().body(requestBody.toString()).post("http://httpbin.org/post");
+    Response requestPostWithBody(HashMap<String, String> dataHashMap) {
+        JSONObject requestBody = new JSONObject(dataHashMap);
+        return   given().body(requestBody.toString()).post(BASE_URL+POST);
     }
 
-    public Response requestPostWithQueryParams(HashMap<String,String> dataHashMap) {
-        return given().queryParams(dataHashMap).post("http://httpbin.org/post");
+    Response requestPostWithQueryParams(HashMap<String, String> dataHashMap) {
+        return given().queryParams(dataHashMap).post(BASE_URL+POST);
     }
 
 
     /**
      * Execute Stream request
      */
-    public void requeatStream() {
+    Response requestStream(int number) {
+        return given().get(BASE_URL+ STREAM, number);
     }
 
     /**
      * Execute Basic-Auth request
      */
-    public void requeatBasicAuth() {
+    Response requestBasicAuth(String validUsr, String validPswd, String actualUsr, String actualPswd) {
+        String authHeader = encodeBase64((actualUsr+ COLON +actualPswd));
+        return given().header(new Header(AUTHORIZATION, BASIC +authHeader)).get(BASE_URL+ BASIC_AUTH,validUsr,validPswd);
     }
+
+    private String encodeBase64(String strToEncode) {
+        return new String(Base64.getEncoder().encode(strToEncode.getBytes()));
+    }
+
 }
 
--- src/test/java/TestHttpBin.java
+++ src/test/java/TestHttpBin.java
@@ -1,6 +1,8 @@
 import io.restassured.http.Header;
 import io.restassured.response.Response;
+import org.testng.Assert;
 import org.testng.annotations.BeforeTest;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import java.util.HashMap;
@@ -8,68 +10,125 @@ import java.util.HashMap;
 import static org.hamcrest.core.IsEqual.equalTo;
 
 public class TestHttpBin {
-    HttpBin objHttpBin;
-    Response response;
+    private static final int SUCCESS_CODE = 200;
+    private static final String HEADERS = "headers.";
+    private static final String JSON = "json.";
+    private static final String ARGS = "args.";
+    private static final char NEW_LINE = '\n';
+    private static final String AUTHENTICATED = "authenticated";
+    private static final String USER = "user";
+    private static final int UNAUTHORIZED = 401;
+    private HttpBin objHttpBin;
 
     @BeforeTest
-    public void setUp(){
+    public void setUp() {
         objHttpBin = new HttpBin();
-        response = null;
     }
-    @Test
-    public void getTest() {
-        HttpBin objHttpBin = new HttpBin();
 
-       Header head =new Header("X-Kek","cheburek");
-      //response = objHttpBin.requestGet(head);
- /*       Assert.assertEquals(200, response.getStatusCode(),"Incorrect code");
-        Assert.assertEquals(response.getBody().toString(), head, "Incorrect header");
-        System.out.println(response.getBody().asString());*/
-//        expect().statusCode(200).body();
-//        response = expect().statusCode(200).body();
+    @DataProvider(name = "HttpBin_getCustomHeader")
+    public Object[][] provideHttpBinGetCustomHeader() {
+        return new Object[][]{
+                {new Header("X-Kek", "cheburek")},
+                {new Header("X-Man", "Wolverine")}
+        };
+    }
 
-        //objHttpBin.requestGet(head).then().statusCode(200).and().assertThat().body(containsString(head.getName()+"\": \""+head.getValue()));
-        //System.out.println(response.getBody().asString());
-        objHttpBin.requestGet(head)
-                .then().statusCode(200)
-                .and().assertThat().body("headers." + head.getName(), equalTo(head.getValue()));
+    @Test(dataProvider = "HttpBin_getCustomHeader")
+    public void testHttpBinRequestGet_getCustomHeader(Header header) {
+        objHttpBin.requestGet(header)
+                .then().statusCode(SUCCESS_CODE)
+                .and().assertThat().body(HEADERS + header.getName(), equalTo(header.getValue()));
 
     }
 
-    @Test
-    public void postTestBody() {
-        HttpBin objHttpBin = new HttpBin();
-        HashMap<String,String> hm = new HashMap<>();
-        hm.put("starost", "neradost");
-        hm.put("molodost", "gadost");
-        hm.put("marazm","neskazhu");
-
-
-        Response response = objHttpBin.requestPost(hm);
-
-        System.out.println(response.getBody().asString());
-        //objHttpBin.requestPostWithQueryParams(hm).then().statusCode(200);//.and().assertThat().body("headers."+head.getName(), equalTo(head.getValue()));
-        hm.forEach((k, v) -> response.then()
-                .assertThat().body("json." + k, equalTo(v)));
+    @DataProvider(name = "HttpBin_checkPostBody")
+    public Object[][] provideHttpBinCheckPostBody() {
+        return new Object[][]{
+                {new HashMap<String, String>() {{
+                    put("starost", "neradost");
+                    put("molodost", "gadost");
+                    put("marazm", "neskazhu");
+                }}},
+                {new HashMap<String, String>() {{
+                    put("mama", "govorit");
+                    put("chtoYa", "osobenniy");
+                }}}
+        };
     }
 
-    @Test
-    public void postTestQueryParams() {
-        HttpBin objHttpBin = new HttpBin();
-        HashMap<String,String> hm = new HashMap<>();
-        hm.put("starost", "neradost");
-        hm.put("molodost", "gadost");
-        hm.put("marazm","neskazhu");
+    @Test(dataProvider = "HttpBin_checkPostBody")
+    public void testHttpBinRequestPostWithBody_checkPostBody(HashMap<String,String> data) {
 
-
-        Response response = objHttpBin.requestPostWithQueryParams(hm);
-
-        System.out.println(response.getBody().asString());
-        response.then().statusCode(200);
-        hm.forEach((k, v) -> response.then()
-                .assertThat().body("args." + k, equalTo(v)));
-
-        //objHttpBin.requestPostWithQueryParams(hm).then().statusCode(200);//.and().assertThat().body("headers."+head.getName(), equalTo(head.getValue()));
+        Response response = objHttpBin.requestPostWithBody(data);
+        data.forEach((k, v) -> response.then()
+                .assertThat().body(JSON + k, equalTo(v)));
     }
 
+    @DataProvider(name = "HttpBin_checkPostWithQueryParams")
+    public Object[][] provideHttpBinCheckPostWithQueryParams() {
+        return new Object[][]{
+                {new HashMap<String, String>() {{
+                    put("starost", "neradost");
+                    put("molodost", "gadost");
+                    put("marazm", "neskazhu");
+                }}},
+                {new HashMap<String, String>() {{
+                    put("mama", "govorit");
+                    put("chtoYa", "osobenniy");
+                }}}
+        };
+    }
+
+    @Test(dataProvider = "HttpBin_checkPostWithQueryParams")
+    public void testHttpBinRequestPostWithQueryParams_checkPostWithQueryParams(HashMap<String,String> queryParams) {
+
+        Response response = objHttpBin.requestPostWithQueryParams(queryParams);
+        response.then().statusCode(SUCCESS_CODE);
+        queryParams.forEach((k, v) -> response.then()
+                .assertThat().body(ARGS + k, equalTo(v)));
+    }
+
+    @DataProvider(name = "HttpBin_checkStreamLinesCount")
+    public Object[][] provideHttpBinCheckStreamLinesCount() {
+        return new Object[][]{
+                {6},
+                {100},
+                {150},
+                {0}
+        };
+    }
+
+    @Test(dataProvider = "HttpBin_checkStreamLinesCount")
+    public void testHttpBinRequestStream_checkStreamLinesCount(int number) {
+        long counter = objHttpBin.requestStream(number).then().statusCode(SUCCESS_CODE)
+                .and().extract().body().asString().chars().filter(sep -> sep == NEW_LINE).count();
+        Assert.assertEquals(counter, number, "Wrong lines number");
+    }
+
+    @DataProvider(name = "HttpBin_checkValidAuth")
+    public Object[][] provideHttpBinCheckValidAuth() {
+        return new Object[][]{
+                {"AllCops", "areBeautiful"},
+                {"RestIn", "Pepperoni"}
+        };
+    }
+
+    @Test(dataProvider = "HttpBin_checkValidAuth")
+    public void testHttpBinRequestBasicAuth_checkValidAuth(String validUsr, String validPswd) {
+        objHttpBin.requestBasicAuth(validUsr, validPswd, validUsr, validPswd)
+                .then().statusCode(SUCCESS_CODE)
+                .and().assertThat().body(AUTHENTICATED, equalTo(true), USER, equalTo(validUsr));
+    }
+    @DataProvider(name = "HttpBin_checkInvalidAuth")
+    public Object[][] provideHttpBinCheckInvalidAuth() {
+        return new Object[][]{
+                {"AllCops", "areBeautiful", "Not", "SoPretty"},
+                {"Make", "America", "Great", "Again"}
+        };
+    }
+    @Test(dataProvider = "HttpBin_checkInvalidAuth")
+    public void basicAuthTestInvalidArgs(String validUsr, String validPswd, String actualUsr, String actualPswd) {
+        objHttpBin.requestBasicAuth(validUsr, validPswd, actualUsr, actualPswd)
+                .then().statusCode(UNAUTHORIZED);
+    }
 }
