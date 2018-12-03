53673d47f6b44c0485f54b36908a87218039c759
AValerievna <a.valerievna12@gmail.com>
2018-12-03 18:23:27 +0300

    post and get test

--- src/test/java/TestHttpBin.java
+++ src/test/java/TestHttpBin.java
@@ -1,16 +1,11 @@
 import io.restassured.http.Header;
 import io.restassured.response.Response;
-import org.json.JSONArray;
-import org.testng.Assert;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
-import java.io.IOException;
 import java.util.HashMap;
 
-import static io.restassured.RestAssured.*;
 import static org.hamcrest.core.IsEqual.equalTo;
-import static org.hamcrest.core.StringContains.containsString;
 
 public class TestHttpBin {
     HttpBin objHttpBin;
@@ -35,7 +30,9 @@ public class TestHttpBin {
 
         //objHttpBin.requestGet(head).then().statusCode(200).and().assertThat().body(containsString(head.getName()+"\": \""+head.getValue()));
         //System.out.println(response.getBody().asString());
-        objHttpBin.requestGet(head).then().statusCode(200).and().assertThat().body("headers."+head.getName(), equalTo(head.getValue()));
+        objHttpBin.requestGet(head)
+                .then().statusCode(200)
+                .and().assertThat().body("headers." + head.getName(), equalTo(head.getValue()));
 
     }
 
@@ -43,31 +40,36 @@ public class TestHttpBin {
     public void postTestBody() {
         HttpBin objHttpBin = new HttpBin();
         HashMap<String,String> hm = new HashMap<>();
-        hm.put("starost'","neradost'");
-        hm.put("molodost'","gadost'");
+        hm.put("starost", "neradost");
+        hm.put("molodost", "gadost");
         hm.put("marazm","neskazhu");
 
 
         Response response = objHttpBin.requestPost(hm);
 
         System.out.println(response.getBody().asString());
-        hm.forEach((k,v) -> objHttpBin.requestPost(hm).assertThat().body("args."+k, equalTo(v)));
-        objHttpBin.requestPost(hm).then().statusCode(200).and().assertThat().body(hm.forEach((k,v) -> ("args."+k, equalTo(v))));
+        //objHttpBin.requestPostWithQueryParams(hm).then().statusCode(200);//.and().assertThat().body("headers."+head.getName(), equalTo(head.getValue()));
+        hm.forEach((k, v) -> response.then()
+                .assertThat().body("json." + k, equalTo(v)));
     }
 
     @Test
     public void postTestQueryParams() {
         HttpBin objHttpBin = new HttpBin();
         HashMap<String,String> hm = new HashMap<>();
-        hm.put("starost'","neradost'");
-        hm.put("molodost'","gadost'");
+        hm.put("starost", "neradost");
+        hm.put("molodost", "gadost");
         hm.put("marazm","neskazhu");
 
 
         Response response = objHttpBin.requestPostWithQueryParams(hm);
 
         System.out.println(response.getBody().asString());
-        objHttpBin.requestPostWithQueryParams(hm).then().statusCode(200);//.and().assertThat().body("headers."+head.getName(), equalTo(head.getValue()));
+        response.then().statusCode(200);
+        hm.forEach((k, v) -> response.then()
+                .assertThat().body("args." + k, equalTo(v)));
+
+        //objHttpBin.requestPostWithQueryParams(hm).then().statusCode(200);//.and().assertThat().body("headers."+head.getName(), equalTo(head.getValue()));
     }
 
 }
